9041687883a154e0860a2efe36da1fd5418465dd
Accompany <accompany_xiong@qq.com>
2020-07-27 17:58:52 +0800

    bug : 又又又又又又又又又写了一堆垃圾代码

--- src/main/java/com/accompnay/work/A2/ZoneWriter.java
+++ src/main/java/com/accompnay/work/A2/ZoneWriter.java
@@ -1,9 +1,6 @@
 package com.accompnay.work.A2;
 
-import org.apache.poi.ss.formula.functions.T;
-
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.util.Map;
 
 public abstract class ZoneWriter {
@@ -19,23 +16,11 @@ public abstract class ZoneWriter {
 
     public abstract void write(Map<String, Zone> zoneMap, BufferedWriter writer) throws Exception;
 
+    private static int[] capacityList = new int[]{1, 2, 2, 3, 4, 5, 5, 6, 7, 7, 7};
+
     protected int getCapacity(int curNum, int maxNum) {
         double p = (double) curNum / maxNum;
-        if (p < 0.1) {
-            return 1;
-        } else if (0.1 <= p && p < 0.3) {
-            return 2;
-        } else if (0.3 <= p && p < 0.4) {
-            return 3;
-        } else if (0.4 <= p && p < 0.5) {
-            return 4;
-        } else if (0.5 <= p && p < 0.7) {
-            return 5;
-        } else if (0.7 <= p && p < 0.8) {
-            return 6;
-        } else if (0.8 <= p) {
-            return 7;
-        }
-        return 0;
+        int index = (int) (p * 10);
+        return capacityList[index];
     }
 }
