1e467ec40efc994adfcb552b55058a2a7adecb7b
accompany <accompany_xiong@qq.com>
2021-08-20 14:13:29 +0800

    fix : 不使用额外空间

--- src/main/java/com/accompnay/swordFingerOffer/CopyRandomList.java
+++ src/main/java/com/accompnay/swordFingerOffer/CopyRandomList.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * 示例 2：
  * 输入：head = [[1,1],[2,1]]
  * 输出：[[1,1],[2,1]]
- *
+ * <p>
  * 剑指 Offer 35. 复杂链表的复制
  * 链接：https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
  */
@@ -56,22 +56,59 @@ public class CopyRandomList {
 		return map.get(head);
 	}
 
+	public Node copyRandomList2(Node head) {
+		if (head == null) {
+			return head;
+		}
+		//1.copy所有节点为当前的节点的next
+		Node cur = head;
+		while (cur != null) {
+			Node newNode = new Node(cur.val);
+			Node next = cur.next;
+			cur.next = newNode;
+			newNode.next = next;
+			cur = next;
+		}
+		//2.将复制出的节点的random指针指向copy节点
+		cur = head;
+		while (cur != null) {
+			Node copyNode = cur.next;
+			Node nextNode = copyNode.next;
+			if (cur.random != null) {
+				copyNode.random = cur.random.next;
+			}
+			cur = nextNode;
+		}
+		//3.将链表脱离
+		Node copyHead = head.next;
+		cur = head;
+		while (cur != null) {
+			Node copyNode = cur.next;
+			Node nextNode = cur.next.next;
+			cur.next = nextNode;
+			copyNode.next = nextNode == null ? null : nextNode.next;
+			cur = nextNode;
+		}
+
+		return copyHead;
+	}
+
 
 	public static void main(String[] args) {
 		Node node = new Node(7);
-		node.next =  new Node(13);
+		node.next = new Node(13);
 		node.random = null;
 
-		node.next.next =  new Node(11);
+		node.next.next = new Node(11);
 		node.next.random = node;
 
-		node.next.next.next =  new Node(10);
-		node.next.next.next.next =  new Node(1);
+		node.next.next.next = new Node(10);
+		node.next.next.next.next = new Node(1);
 		node.next.next.random = node.next.next.next.next;
 		node.next.next.next.random = node.next.next;
-		node.next.next.next.next.random =  node;
+		node.next.next.next.next.random = node;
 		CopyRandomList copyRandomList = new CopyRandomList();
-		Node list = copyRandomList.copyRandomList(node);
+		Node list = copyRandomList.copyRandomList2(node);
 		System.out.println(list);
 	}
 }
