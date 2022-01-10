12329793815043ed2b335a45d24f9174d1205cc4
accompany <accompany_xiong@qq.com>
2022-01-10 11:56:08 +0800

    feature(提交动态规划代码)：

--- src/main/java/com/accompnay/Stormzhang/dynamicProgramming/CoinChange.java
+++ src/main/java/com/accompnay/Stormzhang/dynamicProgramming/CoinChange.java
@@ -1,5 +1,7 @@
 package com.accompnay.Stormzhang.dynamicProgramming;
 
+import java.util.Arrays;
+
 /**
  * 322. 零钱兑换
  * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
@@ -49,6 +51,15 @@ public class CoinChange {
 	}
 
 	public int coinChange(int[] coins, int amount) {
-		return 1;
+		int[] dp = new int[amount + 1];
+		Arrays.fill(dp, amount + 1);
+		dp[0] = 0;
+		for (int i = 0; i < dp.length; i++) {
+			for (int coin : coins) {
+				if (i - coin < 0) continue;
+				dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+			}
+		}
+		return dp[amount] == amount + 1 ? -1 : dp[amount];
 	}
 }
