ebd023ee1e4b4f258437ca9149b449082e9edccc
yaoyaunxiong <yaoyuanxiong@aobi.com>
2022-09-07 17:21:48 +0800

    feature(dp): 提交摔鸡蛋算法

---
+++ src/main/java/com/accompnay/TopicAlgorithms/practiceSet/dp/StoneGame.java
@@ -0,0 +1,126 @@
+package com.accompnay.TopicAlgorithms.practiceSet.dp;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 877. 石子游戏
+ * Alice 和 Bob 用几堆石子在做游戏。
+ * 一共有偶数堆石子，排成一行；每堆都有 正 整数颗石子，数目为 piles[i] 。
+ * <p>
+ * 游戏以谁手中的石子最多来决出胜负。石子的 总数 是 奇数 ，所以没有平局。
+ * <p>
+ * Alice 和 Bob 轮流进行，Alice 先开始 。 每回合，玩家从行的 开始 或 结束 处取走整堆石头。
+ * 这种情况一直持续到没有更多的石子堆为止，此时手中 石子最多 的玩家 获胜 。
+ * <p>
+ * 假设 Alice 和 Bob 都发挥出最佳水平，当 Alice 赢得比赛时返回 true ，当 Bob 赢得比赛时返回 false 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：piles = [5,3,4,5]
+ * 输出：true
+ * 解释：
+ * Alice 先开始，只能拿前 5 颗或后 5 颗石子 。
+ * 假设他取了前 5 颗，这一行就变成了 [3,4,5] 。
+ * 如果 Bob 拿走前 3 颗，那么剩下的是 [4,5]，Alice 拿走后 5 颗赢得 10 分。
+ * 如果 Bob 拿走后 5 颗，那么剩下的是 [3,4]，Alice 拿走后 4 颗赢得 9 分。
+ * 这表明，取前 5 颗石子对 Alice 来说是一个胜利的举动，所以返回 true 。
+ * 示例 2：
+ * <p>
+ * 输入：piles = [3,7,2,3]
+ * 输出：true
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= piles.length <= 500
+ * piles.length 是 偶数
+ * 1 <= piles[i] <= 500
+ * sum(piles[i]) 是 奇数
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/stone-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class StoneGame {
+
+	public static void main(String[] args) {
+		StoneGame stoneGame = new StoneGame();
+		boolean b = stoneGame.stoneGame2(new int[]{2, 8, 3, 5});
+		System.out.println(b);
+	}
+
+	public boolean stoneGame2(int[] piles) {
+		int n = piles.length;
+		int[][][] dp = new int[n][n][2];
+		for (int i = 0; i < n; i++) {
+			dp[i][i][0] = piles[i];
+			dp[i][i][1] = 0;
+		}
+		int row = dp.length - 2;
+		while (row >= 0) {
+			int j = dp.length - 1;
+			int i = row;
+			while (i >= 0 && j >= 0) {
+				int a = dp[i][j - 1][1] + piles[j];
+				int b = dp[i + 1][j][1] + piles[i];
+				if (a > b) {
+					dp[i][j][0] = a;
+					dp[i][j][1] = dp[i][j - 1][0];
+				} else {
+					dp[i][j][0] = b;
+					dp[i][j][1] = dp[i + 1][j][0];
+				}
+				j--;
+				i--;
+			}
+			row--;
+		}
+		return dp[0][n - 1][0] - dp[0][n - 1][1] > 0;
+	}
+
+	public boolean stoneGame(int[] piles) {
+		for (int pile : piles) {
+			mun += pile;
+		}
+		mun = mun / 2;
+		return dp(piles, 0, piles.length - 1, true);
+	}
+
+	int mun = 0;
+	int aValue = 0;
+	int bValue = 0;
+	private Set<String> memo = new HashSet<>();
+
+	private boolean dp(int[] piles, int start, int end, boolean flag) {
+		if (start >= piles.length || start > end || end <= 0) {
+			return aValue > bValue;
+		}
+		if (aValue >= mun) {
+			return true;
+		}
+		if (bValue >= mun) {
+			return false;
+		}
+		String key = start + "#" + end + "#" + flag;
+		if (memo.contains(key)) {
+			return false;
+		}
+		aValue = flag ? aValue + piles[start] : aValue;
+		bValue = !flag ? bValue + piles[start] : bValue;
+		if (dp(piles, start + 1, end, !flag)) {
+			return true;
+		}
+		aValue = flag ? aValue - piles[start] : aValue;
+		bValue = !flag ? bValue - piles[start] : bValue;
+
+		aValue = flag ? aValue + piles[end] : aValue;
+		bValue = !flag ? bValue + piles[end] : bValue;
+		if (dp(piles, start, end - 1, !flag)) {
+			return true;
+		}
+		aValue = flag ? aValue - piles[end] : aValue;
+		bValue = !flag ? bValue - piles[end] : bValue;
+		memo.add(key);
+		return false;
+	}
+}
---
+++ src/main/java/com/accompnay/TopicAlgorithms/practiceSet/dp/SuperEggDrop.java
@@ -0,0 +1,118 @@
+package com.accompnay.TopicAlgorithms.practiceSet.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 887. 鸡蛋掉落
+ * 给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
+ * <p>
+ * 已知存在楼层 f， ，满足 0 <= f <= n 任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
+ * <p>
+ * 每次操作，你可以取一枚没有碎的鸡蛋并把它从任一楼层 x 扔下（满足 1 <= x <= n）。如果鸡蛋碎了，你就不能再次使用它。
+ * 如果某枚鸡蛋扔下后没有摔碎，则可以在之后的操作中 重复使用 这枚鸡蛋。
+ * <p>
+ * 请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：k = 1, n = 2
+ * 输出：2
+ * 解释：
+ * 鸡蛋从 1 楼掉落。如果它碎了，肯定能得出 f = 0 。
+ * 否则，鸡蛋从 2 楼掉落。如果它碎了，肯定能得出 f = 1 。
+ * 如果它没碎，那么肯定能得出 f = 2 。
+ * * 因此，在最坏的情况下我们需要移动 2 次以确定 f 是多少。
+ * 示例 2：
+ * <p>
+ * 输入：k = 2, n = 6
+ * 输出：3
+ * 示例 3：
+ * <p>
+ * 输入：k = 3, n = 14
+ * 输出：4
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= k <= 100
+ * 1 <= n <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/super-egg-drop
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SuperEggDrop {
+	public static void main(String[] args) {
+		SuperEggDrop superEggDrop = new SuperEggDrop();
+		int i = superEggDrop.superEggDrop(3, 14);
+		System.out.println(i);
+	}
+
+
+	public int superEggDrop(int k, int n) {
+		return dp2(k, n);
+	}
+
+	private Map<Integer, Integer> memo = new HashMap<>();
+
+	private int dp(int k, int n) {
+		if (k == 1) {
+			return n;
+		}
+		if (n == 0) {
+			return 0;
+		}
+		Integer key = k * 100000 + n;
+		if (memo.containsKey(key)) {
+			return memo.get(key);
+		}
+		int res = Integer.MAX_VALUE;
+		for (int i = 1; i <= n; i++) {
+			int dp = dp(k - 1, i - 1);
+			int dp1 = dp(k, n - i);
+			res = Math.min(Math.max(dp, dp1) + 1, res);
+		}
+		memo.put(key, res);
+		return res;
+	}
+
+	private Map<Integer, Integer> memo2 = new HashMap<>();
+
+	private int dp2(int k, int n) {
+		if (k == 1) {
+			return n;
+		}
+		if (n == 0) {
+			return 0;
+		}
+		Integer key = k * 100000 + n;
+		if (memo2.containsKey(key)) {
+			return memo.get(key);
+		}
+		int left = 1;
+		int right = n;
+		while (left + 1 < right) {
+			int mid = (left + right) >> 1;
+			int lowValue = dp2(k - 1, mid - 1);
+			int highVal = dp2(k, n - mid);
+			if (lowValue > highVal) {
+				right = mid;
+			} else if (lowValue < highVal) {
+				left = mid;
+			} else {
+				left = right = mid;
+			}
+		}
+		int res = 0;
+		if (left == right) {
+			res = 1 + Math.max(dp2(k - 1, left - 1), dp2(k, n - left));
+		} else {
+			int maxLeft = Math.max(dp2(k - 1, left - 1), dp2(k, n - left));
+			int maxRight = Math.max(dp2(k - 1, right - 1), dp2(k, n - right));
+			res = 1 + Math.min(maxLeft, maxRight);
+		}
+		memo.put(key, res);
+		return res;
+	}
+
+}
