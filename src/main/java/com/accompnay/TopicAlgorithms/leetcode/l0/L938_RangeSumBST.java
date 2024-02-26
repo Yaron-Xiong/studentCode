47983f09cb4a677e040357d5e3a11d63daf4e3b3
Yaron-Xiong <Accompany_Xiong@qq.com>
2024-02-26 18:39:48 +0800

    feature: 1.提交leetcode

---
+++ src/main/java/com/accompnay/TopicAlgorithms/leetcode/l0/L938_RangeSumBST.java
@@ -0,0 +1,66 @@
+package com.accompnay.TopicAlgorithms.leetcode.l0;
+
+/**
+ * 938. 二叉搜索树的范围和
+ * 简单
+ * 相关标签
+ * 相关企业
+ * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+ * 输出：32
+ * 示例 2：
+ * <p>
+ * 输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+ * 输出：23
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [1, 2 * 104] 内
+ * 1 <= Node.val <= 105
+ * 1 <= low <= high <= 105
+ * 所有 Node.val 互不相同
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/range-sum-of-bst/description/?envType=daily-question&envId=2024-02-26">...</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class L938_RangeSumBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val >= low && root.val <= high) {
+            int res = root.val;
+            res += rangeSumBST(root.right, low, high);
+            res += rangeSumBST(root.left, low, high);
+            return res;
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        return 0;
+    }
+}
